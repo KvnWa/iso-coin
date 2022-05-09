ff7301f26a862920bd08d043c38bdbef7eb5ac30
Kevin Wang <kevqwang@gmail.com>
2022-05-09 15:14:03 -0400

    first store reducer

--- src/index.js
+++ src/index.js
@@ -5,17 +5,18 @@ import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
+import store from './app/store';
 
 
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-
-        <App />
-     
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root'),
+    <React.StrictMode>
+        <Router>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </Router>
+    </React.StrictMode>,
+    document.getElementById('root'),
 );
